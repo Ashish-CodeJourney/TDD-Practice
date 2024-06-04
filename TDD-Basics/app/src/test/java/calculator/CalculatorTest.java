6a037f1f01a6081e2269dad7d34379aad0b179e5
Ashish Vaghela <ashish.vaghela@egreycell.com>
2024-06-04 19:06:51 +0530

    testcase for `sub` function

--- TDD-Basics/app/src/test/java/calculator/CalculatorTest.java
+++ TDD-Basics/app/src/test/java/calculator/CalculatorTest.java
@@ -11,4 +11,11 @@ public class CalculatorTest {
         int result = calculator.add(5, 3);
         assertEquals(8, result, "5 + 3 should equal 8");
     }
+
+    @Test
+    void testSub() {
+        Calculator calculator = new Calculator();
+        int result = calculator.sub(20,5);
+        assertEquals(15, result, "20 - 5 should equal 15");
+    }
 }
