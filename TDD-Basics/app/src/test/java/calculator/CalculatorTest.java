7b8b54f1158639f402e4112693fdb9823e0ad2bc
Ashish Vaghela <ashish.vaghela@egreycell.com>
2024-06-04 18:58:15 +0530

    testcase for `add` function

--- TDD-Basics/app/src/test/java/calculator/CalculatorTest.java
+++ TDD-Basics/app/src/test/java/calculator/CalculatorTest.java
@@ -1,8 +1,14 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
-    
+
+    @Test
+    void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(5, 3);
+        assertEquals(8, result, "5 + 3 should equal 8");
+    }
 }
