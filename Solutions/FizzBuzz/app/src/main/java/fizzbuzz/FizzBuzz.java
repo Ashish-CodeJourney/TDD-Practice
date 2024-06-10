713be8029fc76ea2e25c6779fef4ef52752d0ffa
Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-06-10 18:12:25 +0530

    Implementation for 'FIzz' Condition

--- Solutions/FizzBuzz/app/src/main/java/fizzbuzz/FizzBuzz.java
+++ Solutions/FizzBuzz/app/src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,10 @@
 package fizzbuzz;
 
 public class FizzBuzz {
+    public String play(int number) {
+        if(number % 3 == 0){
+            return "Fizz";
+        }
+        return "";
+    }
 }
