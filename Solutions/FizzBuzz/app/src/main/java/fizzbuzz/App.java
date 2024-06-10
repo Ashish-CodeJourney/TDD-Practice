59ddb0e329606393ecb5b08a5948d81e1f28e0e5
Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-06-10 18:50:54 +0530

    Exception Handling in Main Class for Invalid Type of Input

--- Solutions/FizzBuzz/app/src/main/java/fizzbuzz/App.java
+++ Solutions/FizzBuzz/app/src/main/java/fizzbuzz/App.java
@@ -1,17 +1,22 @@
 package fizzbuzz;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int number;
         System.out.println("Enter a Number :");
         Scanner sc = new Scanner(System.in);
 
-
-        number = sc.nextInt();
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        System.out.println(fizzbuzz.play(number));
+        try {
+            number = sc.nextInt();
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            System.out.println(fizzbuzz.play(number));
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Please enter Integer Value");
+        }
     }
 }
