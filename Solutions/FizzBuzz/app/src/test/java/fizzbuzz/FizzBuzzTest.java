7e3d51a4502c0124cc7e57c51110128036bf23f2
Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-06-10 17:36:14 +0530

    TestCase for 'Fizz' Condition

--- Solutions/FizzBuzz/app/src/test/java/fizzbuzz/FizzBuzzTest.java
+++ Solutions/FizzBuzz/app/src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -1,7 +1,16 @@
 package fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
 
+    @Test
+    void testFizz(){
+        var fizzbuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzbuzz.play(3));
+        assertEquals("Fizz", fizzbuzz.play(6));
+        assertEquals("Fizz", fizzbuzz.play(9));
+    }
 }
