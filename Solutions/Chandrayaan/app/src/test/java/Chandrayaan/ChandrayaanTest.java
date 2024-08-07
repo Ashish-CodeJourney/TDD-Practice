370d1912c18f48d64fd0354206026da25adada7f
@Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-08-07 15:55:00 +0530

    testMoveForwardFromEast TestCase Added

--- Solutions/Chandrayaan/app/src/test/java/Chandrayaan/ChandrayaanTest.java
+++ Solutions/Chandrayaan/app/src/test/java/Chandrayaan/ChandrayaanTest.java
@@ -15,4 +15,15 @@ public class ChandrayaanTest {
         assertEquals(0, spacecraft.getZ());
         assertEquals("N", spacecraft.getDirection());
     }
+
+    @Test
+    void testMoveForwardFromEast () {
+        Chandrayaan spacecraft = new Chandrayaan(0, 0, 0, "E");
+        spacecraft.executeCommand('f');
+        assertEquals(1, spacecraft.getX());
+        assertEquals(0, spacecraft.getY());
+        assertEquals(0, spacecraft.getZ());
+        assertEquals("E", spacecraft.getDirection());
+    }
+
 }
