2451ff1d315b97e27209980c709098aafdb8ffb9
@Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-08-07 15:50:10 +0530

    Implementation to pass testMoveForwardFromNorth Testcase

--- Solutions/Chandrayaan/app/src/main/java/Chandrayaan/Chandrayaan.java
+++ Solutions/Chandrayaan/app/src/main/java/Chandrayaan/Chandrayaan.java
@@ -1,4 +1,40 @@
 package Chandrayaan;
 
 public class Chandrayaan {
+
+    private int x;
+    private int y;
+    private int z;
+    private String direction;
+
+    public Chandrayaan(int x, int y, int z, String direction) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.direction = direction;
+    }
+
+    public void executeCommand(char command) {
+        if (command == 'f') {
+            if (direction.equals("N")) {
+                y += 1;
+            }
+        }
+    }
+
+    public int getX () {
+        return x;
+    }
+
+    public int getY () {
+        return y;
+    }
+
+    public int getZ () {
+        return z;
+    }
+
+    public String getDirection () {
+        return direction;
+    }
 }
