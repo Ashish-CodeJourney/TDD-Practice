d5746668ab5f402d1667abb51fd09bfb70438f9e
@Ashish-CodeJourney <vaghelaashish111@gmail.com>
2024-08-07 16:04:32 +0530

    Implementation to pass Remaining TestCases

--- Solutions/Chandrayaan/app/src/main/java/Chandrayaan/Chandrayaan.java
+++ Solutions/Chandrayaan/app/src/main/java/Chandrayaan/Chandrayaan.java
@@ -15,6 +15,29 @@ public class Chandrayaan {
     }
 
     public void executeCommand(char command) {
+        switch (command) {
+            case 'f':
+                moveForward();
+                break;
+            case 'b':
+                moveBackward();
+                break;
+            case 'l':
+                turnLeft();
+                break;
+            case 'r':
+                turnRight();
+                break;
+            case 'u':
+                turnUp();
+                break;
+            case 'd':
+                turnDown();
+                break;
+        }
+    }
+    
+    private void moveForward() {
         switch (direction) {
             case "N":
                 y += 1;
@@ -22,8 +45,86 @@ public class Chandrayaan {
             case "E":
                 x += 1;
                 break;
+            case "S":
+                y -= 1;
+                break;
+            case "W":
+                x -= 1;
+                break;
+            case "U":
+                z += 1;
+                break;
+            case "D":
+                z -= 1;
+                break;
         }
     }
+    
+    private void moveBackward() {
+        switch (direction) {
+            case "N":
+                y -= 1;
+                break;
+            case "E":
+                x -= 1;
+                break;
+            case "S":
+                y += 1;
+                break;
+            case "W":
+                x += 1;
+                break;
+            case "U":
+                z -= 1;
+                break;
+            case "D":
+                z += 1;
+                break;
+        }
+    }
+    
+    private void turnLeft() {
+        switch (direction) {
+            case "N":
+                direction = "W";
+                break;
+            case "E":
+                direction = "N";
+                break;
+            case "S":
+                direction = "E";
+                break;
+            case "W":
+                direction = "S";
+                break;
+        }
+    }
+    
+    private void turnRight() {
+        switch (direction) {
+            case "N":
+                direction = "E";
+                break;
+            case "E":
+                direction = "S";
+                break;
+            case "S":
+                direction = "W";
+                break;
+            case "W":
+                direction = "N";
+                break;
+        }
+    }
+    
+    private void turnUp() {
+        direction = "U";
+    }
+    
+    private void turnDown() {
+        direction = "D";
+    }
+    
 
     public int getX () {
         return x;
